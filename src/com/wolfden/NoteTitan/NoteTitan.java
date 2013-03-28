0ed07a6d43df037dfeee480f7704cee173e08dff
Nish Tahir <wolfdenstudios@live.com>
2013-03-29 00:28:17 +0800

    initializing project

---
+++ src/com/wolfden/NoteTitan/NoteTitan.java
@@ -0,0 +1,13 @@
+package com.wolfden.NoteTitan;
+
+public class NoteTitan {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
